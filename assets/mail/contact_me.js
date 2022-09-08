ab918135745cf19d93cc32df589b821ff353780e
AElimam <79451342+AElimam@users.noreply.github.com>
2022-09-08 16:12:40 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://yuwprx8hvj.execute-api.eu-west-1.amazonaws.com/default/cc-predict-lambda",
+                url: "https://yuwprx8hvj.execute-api.eu-west-1.amazonaws.com/default/cclambda2",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
